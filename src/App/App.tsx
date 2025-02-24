18b303e61ec022395ac44e73f897704270528a25
Quyen-Peter <quyentrinh040@gmail.com>
2025-02-24 10:30:26 +0700

    dasboard of peter

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MenuLeftTwo from "../image-convert-app/src/components/menuCompos/menuLeftTwo";
+import TrashList from '../components/dashboard/TrashList';
+
+
 export default function App() {
   return (
     <View style={styles.container}>  
-        <MenuLeftTwo/>
+        <TrashList/>
     </View>
   );
 }
