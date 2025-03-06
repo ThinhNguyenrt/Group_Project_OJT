7a6e051974a23a15dc12f44df9f91305ca921c2a
trangtanduoc <trangtanduoc@gmail.com>
2025-03-06 13:11:51 +0700

    buid android

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import HomeScreen from '../actions/home';
 import DashThree from '../components/dashboard/dashThree';
+import AiEdit from '../components/generate/aiEdit';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,8 @@ export default function App() {
   
   return (
       <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
+          <Stack.Navigator initialRouteName={Platform.OS === "web" ? "Home" : "AiEdit"}>
+            <Stack.Screen name="AiEdit" component={AiEdit} options={{ headerShown: false }}/>
             <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
             <Stack.Screen name="Detail" component={DashThree} />
           </Stack.Navigator>
